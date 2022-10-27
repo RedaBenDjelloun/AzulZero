ea545ca2b5e279ad33fc72f0c9ce7fb0fc793993
RedaBenDjelloun <reda.bendjelloun@gmail.com>
2022-10-27 17:13:02 +0200

    First tries on images

--- GUI.h
+++ GUI.h
@@ -3,12 +3,19 @@
 #include <iostream>
 using namespace std;
 #include <Imagine/Graphics.h>
+#include <Imagine/Images.h>
 using namespace Imagine;
+
+// GUI Parameters
+
+const int window_width = 500;
+const int window_height = 500;
 
 // Tiles
 
-const char* BLUE = srcPath("Images/blue.svg");
-const char* YELLOW = srcPath("Images/yellow.svg");
-const char* RED = srcPath("Images/red.svg");
-const char* BLACK = srcPath("Images/black.svg");
-const char* CYAN = srcPath("Images/cyan.svg");
+const string BLUE_TILE = stringSrcPath("Images/blue.svg");
+const string YELLOW_TILE = stringSrcPath("Images/yellow.svg");
+const string RED_TILE = stringSrcPath("Images/red.svg");
+const string BLACK_TILE = stringSrcPath("Images/black.svg");
+const string CYAN_TILE = stringSrcPath("Images/cyan.svg");
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
+#include "GUI.h"
+
 int main(){
+    Image<AlphaColor> image;
+
+    if(! load(image, RED_TILE)) {
+        cerr << "Error loading image file " << RED_TILE << endl;
+        return 1;
+    }
+
+    openWindow(window_width, window_height);
+    display(image,0,0);
+
+    endGraphics();
     return 0;
+
 }
 
