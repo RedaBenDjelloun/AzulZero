a8254dc0466044d9c07c2c176e61f618b77c18a6
RedaBenDjelloun <reda.bendjelloun@gmail.com>
2022-10-27 16:17:04 +0200

    First commit on branch GUI : files and colors

---
+++ GUI.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+#include <Imagine/Graphics.h>
+using namespace Imagine;
+
+// Tiles
+
+const char* BLUE = srcPath("Images/blue.svg");
+const char* YELLOW = srcPath("Images/yellow.svg");
+const char* RED = srcPath("Images/red.svg");
+const char* BLACK = srcPath("Images/black.svg");
+const char* CYAN = srcPath("Images/cyan.svg");
